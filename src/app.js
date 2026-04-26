b9ed7abd6d5538c974af6b75634f434dddbc6ee6
Sunil Ghate <05510@sdloffice.com>
2026-04-26 18:34:21 +0530

    Debuging apis

--- src/app.js
+++ src/app.js
@@ -9,8 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log(" app.js loaded");
+
+app.use((req, res, next) => {
+  console.log("👉", req.method, req.url);
+  next();
+});
+
+app.post("/test", (req, res) => {
+  res.json({ message: "Test route working" });
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/telegram", telegramRoutes);
+
+
 
 app.get("/", (req, res) => {
   res.send("API is running...");
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
+console.log("Auth routes loaded");
+
 router.post("/login", authController.login);
 router.post("/register", authController.register); // NEW
 router.post("/refresh", authController.refresh);
